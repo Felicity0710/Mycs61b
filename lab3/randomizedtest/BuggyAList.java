ac266ee51c121b0897d7ef8df384e243e1fd82d6
Felicity0710 <164110973+Felicity0710@users.noreply.github.com>
2025-05-28 20:28:33 +0800

    Add files via upload

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -79,7 +79,7 @@ public class BuggyAList<Item> {
         Item x = getLast();
         size = size - 1;
         if ((size < items.length / 4) && (size > 4)) {
-            resize(size / 4);
+            resize(items.length / 4);
         }
         return x;
     }
