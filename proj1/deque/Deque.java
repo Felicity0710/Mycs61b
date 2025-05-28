7df96f24dbd6ec6f221e614fe04d294dee104fb8
Felicity0710 <164110973+Felicity0710@users.noreply.github.com>
2025-05-28 10:18:02 +0800

    add Deque.java

---
+++ proj1/deque/Deque.java
@@ -0,0 +1,22 @@
+package deque;
+
+public interface Deque<T> extends Iterable<T> {
+    void addFirst(T t);
+
+    T removeFirst();
+
+    void addLast(T t);
+
+    T removeLast();
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    int size();
+
+    T get(int idx);
+
+    void printDeque();
+
+}
