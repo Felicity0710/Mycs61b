19e8bc9db471ec2d2252bc2a4db9e35ca2868923
Felicity0710 <164110973+Felicity0710@users.noreply.github.com>
2025-06-10 14:53:19 +0800

    revise findLCA with parent2

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -122,30 +122,47 @@ public class Commit implements Serializable {
         return hashCode;
     }
 
-    public static String findLCA(String commitID1, String commitID2) {
-        Commit commit1 = fromFile(commitID1), commit2 = fromFile(commitID2);
+    public static String findLCAHelper(String commitId1, String commitId2) {
+        Commit commit1 = fromFile(commitId1), commit2 = fromFile(commitId2);
         if (commit1.depth < commit2.depth) {
             Commit tempCommit = commit1;
             commit1 = commit2;
             commit2 = tempCommit;
-            String tempString = commitID1;
-            commitID1 = commitID2;
-            commitID2 = tempString;
+            String tempString = commitId1;
+            commitId1 = commitId2;
+            commitId2 = tempString;
         }
         while (commit1.depth > commit2.depth) {
-            commitID1 = commit1.parent1;
-            commit1 = fromFile(commitID1);
+            commitId1 = commit1.parent1;
+            commit1 = fromFile(commitId1);
         }
-        if (commitID1.equals(commitID2)) {
-            return commitID1;
+        if (commitId1.equals(commitId2)) {
+            return commitId1;
         }
         while (!commit1.parent1.equals(commit2.parent1)) {
-            commitID1 = commit1.parent1;
-            commitID2 = commit2.parent1;
-            commit1 = fromFile(commitID1);
-            commit2 = fromFile(commitID2);
+            commitId1 = commit1.parent1;
+            commitId2 = commit2.parent1;
+            commit1 = fromFile(commitId1);
+            commit2 = fromFile(commitId2);
         }
         return commit1.parent1;
+    }
+
+    public static String findLCA(String commitID1, String commitID2) {
+        Commit commit1 = fromFile(commitID1), commit2 = fromFile(commitID2);
+        String LCA = findLCAHelper(commitID1, commitID2);
+        if (commitID1.equals(LCA)) {
+            return LCA;
+        }
+        if (!commit1.parent2.isEmpty()) {
+            Commit LatestCommonAncestorParent1Parent2 = fromFile(findLCAHelper(commit1.parent1, commit1.parent2));
+            String LatestCommonAncestorParent2 = findLCAHelper(commit1.parent2, commitID2);
+            Commit LCAParent2 = fromFile(LatestCommonAncestorParent2);
+            if (LatestCommonAncestorParent1Parent2.depth < LCAParent2.depth) {
+                return LatestCommonAncestorParent2;
+            }
+        }
+        return LCA;
     }
 
     public Set<Map.Entry<String, String>> entrySet() {
